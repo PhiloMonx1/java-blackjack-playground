3a69ff92c1f725c4df7aeada33431c525cf639c3
EH13 <philomon@benecl.com>
2024-04-02 13:36:44 +0900

    Feat(함수형프로그래밍) : 스트림 실습 2 완료. *add : `StreamStudy::printLongestWordTop100()` 구현 코드 추가

--- src/main/java/nextstep/fp/StreamStudy.java
+++ src/main/java/nextstep/fp/StreamStudy.java
@@ -27,7 +27,15 @@ public class StreamStudy {
                 .get("src/main/resources/fp/war-and-peace.txt")), StandardCharsets.UTF_8);
         List<String> words = Arrays.asList(contents.split("[\\P{L}]+"));
 
-        // TODO 이 부분에 구현한다.
+	    List<String> longestWords = words.stream()
+			    .filter(word -> word.length() > 12)
+			    .distinct()
+			    .limit(100)
+			    .sorted((word1, word2) -> word2.length() - word1.length())
+			    .map(word -> word.toLowerCase())
+			    .collect(Collectors.toList());
+
+		longestWords.forEach(System.out::println);
     }
 
     public static List<Integer> doubleNumbers(List<Integer> numbers) {
