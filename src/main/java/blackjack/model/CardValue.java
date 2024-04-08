09d13f3dbe60efdad74087fc3c62935ea7d8b2cf
EH13 <philomonx1@gmail.com>
2024-04-08 17:42:38 +0900

    Refactor(블랙잭) : 문자열 포장

--- src/main/java/blackjack/model/CardValue.java
+++ src/main/java/blackjack/model/CardValue.java
@@ -2,11 +2,12 @@ package blackjack.model;
 
 class CardValue {
 
+	private static final String ERROR_MESSAGE = "카드의 값은 1~13 사이의 정수여야 합니다.";
 	private final int value;
 
 	CardValue(int value) {
 		if (value < 1 || value > 13) {
-			throw new IllegalArgumentException("카드의 값은 1~13 사이의 정수여야 합니다.");
+			throw new IllegalArgumentException(ERROR_MESSAGE);
 		}
 		this.value = value;
 	}
--- src/main/java/blackjack/model/Util.java
+++ src/main/java/blackjack/model/Util.java
@@ -1,7 +1,8 @@
 package blackjack.model;
 
 public abstract class Util {
+	private final static String INFORMATION = "는 유틸리티 클래스입니다.";
 	protected Util() {
-		throw new IllegalStateException(this.getClass().getSimpleName() + "는 유틸리티 클래스입니다.");
+		throw new IllegalStateException(this.getClass().getSimpleName() + INFORMATION);
 	}
 }
